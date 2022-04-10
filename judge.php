b1913735065b2dceabac826af2da7c57cc2bba05
WoziKing <wzq23t@163.com>
2022-04-10 15:27:00 +0800

    upload

--- judge.php
+++
@@ -1,9 +0,0 @@
-<?php
-if($_GET['name']=='admin' && $_GET['pwd']=='123456'){
-    echo '<script>window.location.href="./index1.html";</script>';
-    exit();
-}else{
-    echo '<script>alert("账号或密码错误");window.location.href="index.html";</script>';
-    exit();
-}
-?>
